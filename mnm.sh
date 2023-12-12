7d8aaf94a761a8381996fdcb5259a06f960953d0
mylhyz <mylhyz@gmail.com>
2023-12-12 20:37:11 +0800

    update

--- mnm.sh
+++ mnm.sh
@@ -1,7 +1,14 @@
 { # this ensures the entire script is downloaded #
 
-mnm() {
+mnm_echo() {
+  command printf %s\\n "$*" 2>/dev/null
+}
 
+mnm() {
+  if [ "$#" -lt 1 ]; then
+    mnm --help
+    return
+  fi
 }
 
 } # this ensures the entire script is downloaded #
